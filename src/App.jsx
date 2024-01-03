b6eddb8071d916783698e9f357686fab4ada7e3b
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-01-02 20:09:57 -0800

    Create and style card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="container">
+          <img src="/images/illustration-article.svg" alt="Illustration" />
+          <button className="tag">Learning</button>
+          <div className="date">Published 21 Dec 2023</div>
+          <a href="#" className="title">
+            HTML & CSS foundations
+          </a>
+          <div className="article">
+            These languages are the backbone of every website, definine
+            structure, content, and presentation.
+          </div>
+          <div className="author-container">
+            <img
+              src="/images/image-avatar.webp"
+              alt="Author"
+              className="avatar"
+            />
+            <div className="author-name">Greg Hooper</div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
